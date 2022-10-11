38119ada86b803cec23eb5facf00056c2d1932d8
Tyeth Gundry <tyethgundry@googlemail.com>
2022-10-11 11:41:18 +0100

    messing with images/VEML7700

--- LilyGO_tempsensor_hothead/LilyGO_tempsensor_hothead.ino
+++ LilyGO_tempsensor_hothead/LilyGO_tempsensor_hothead.ino
@@ -13,25 +13,29 @@
 #include <Wire.h>
 #include "TFT_eSPI.h"
 #include "hothead.h"
+#include <Adafruit_VEML7700.h>
 
-float cTemp = 12.3;
+float cTemp = 0.0;
+uint8_t lux = 0;
 unsigned int data[2];
 
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
 void setup() {
 	Serial.begin(115200);  // be sure to set USB CDC On Boot: "Enabled"
   Wire.begin(43, 44);  //SDA, SCL
+  veml.begin(&Wire);
   tft.init();
   tft.setRotation(3);
   tft.setSwapBytes(true);
-  tft.fillScreen(TFT_WHITE);  //horiz / vert<> position/dimension
-  tft.pushImage(165, 0, 155, 170, hothead);
+  //tft.fillScreen(TFT_WHITE);  //horiz / vert<> position/dimension
+  tft.pushImage(0, 0, 320, 170, hothead);
   tft.setTextSize(1);
   sprite.createSprite(146, 170);
 
-  sprite.setTextColor(TFT_BLACK, TFT_WHITE);
+  //sprite.setTextColor(TFT_BLACK, TFT_WHITE);
   sprite.setTextDatum(4);
   delay(500);
   Serial.println("In setup!");
@@ -50,24 +54,28 @@ void loop() {
   Wire.requestFrom(0x44, 2);
   data[0] = Wire.read();
   data[1] = Wire.read();
+  
   delay(50);
+  
   cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
 
-  sprite.fillSprite(TFT_WHITE);   // left side background colour
+  lux = veml.autoLux();
+
+  //sprite.fillSprite(TFT_WHITE);   // left side background colour
   sprite.setFreeFont(&Orbitron_Light_24);
   if (!digitalRead(0) && !digitalRead(14))  // reverse logic push for "0"
   {
-    sprite.drawString(" BoneHead!", 75, 16);
+    sprite.drawString("!:" + String(lux,0), 75, 16);
   }
   else {
-    sprite.drawString("HotHead!", 75, 16);
+    sprite.drawString("l:" + String(lux,0), 75, 16);
   }
   sprite.setFreeFont(&Orbitron_Light_32);
-  sprite.setTextColor(TFT_BLUE, TFT_WHITE);
+  //sprite.setTextColor(TFT_BLUE, TFT_WHITE);
   sprite.drawString(String(cTemp, 1) + "C", 75, 54);
 
-  sprite.drawRect(5, 124, 65, 22, TFT_BLACK);  //"left" and "right" text boxes
-  sprite.drawRect(80, 124, 65, 22, TFT_BLACK);
+  //sprite.drawRect(5, 124, 65, 22, TFT_BLACK);  //"left" and "right" text boxes
+  //sprite.drawRect(80, 124, 65, 22, TFT_BLACK);
   if (digitalRead(0)) {                      //  normally "on" level "true".
     sprite.drawString("left pin0", 35, 134, 2);//"Push" is active, level "false"
   }
